6776b7dd5e1a9f0bc9ab92d51e7118919d013c37
tah96 <tylerandrewhunt96@gmail.com>
2022-01-02 11:01:45 -0500

    Fixed hitting object creation script and re-uploaded CSV. Updated script to include necessary calculations. QA testing needed

--- sql_scripts/hitting_object.sql
+++ sql_scripts/hitting_object.sql
@@ -1,24 +1,34 @@
-DROP TABLE IF EXISTS hitting_stats;
+CREATE TABLE public.hitting_stats
+(
+    "gameID" character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    "playerID" character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    "playerPos" character varying(10) COLLATE pg_catalog."default" NOT NULL,
+	"pAtBats" integer NOT NULL,
+	"pRuns" integer NOT NULL,
+	"pHits" integer NOT NULL,
+    "pRBI" integer NOT NULL,
+	"pBB" integer NOT NULL,
+    "pStrikeouts" integer NOT NULL,
+	"pPlateApps" integer NOT NULL,
+    "p2B" integer NOT NULL,
+    "p3B" integer NOT NULL,
+    "pHR" integer NOT NULL,
+    "pTB" integer NOT NULL,
+	"pHBP" integer NOT NULL,
+    "pIntBB" integer NOT NULL,
+    "pSF" integer NOT NULL,
+    CONSTRAINT "pk_Hitting_Stats" PRIMARY KEY ("gameID", "playerID"),
+    CONSTRAINT "fk_Hitting_Stats_gameID" FOREIGN KEY ("gameID")
+        REFERENCES public.game ("gameID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "fk_Hitting_Stats_playerID" FOREIGN KEY ("playerID")
+        REFERENCES public.player ("playerID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
-CREATE TABLE hitting_stats (
-    "gameID" varchar(255)   NOT NULL,
-    "playerID" varchar(10)   NOT NULL,
-    "playerPos" varchar(10)   NOT NULL,
-    "pPlateApp" int   NOT NULL,
-    "pAtBats" int   NOT NULL,
-    "pHits" int   NOT NULL,
-    "pRuns" int   NOT NULL,
-    "pRBI" int   NOT NULL,
-    "pSrikeouts" int   NOT NULL,
-    "p2B" int   NOT NULL,
-    "p3B" int   NOT NULL,
-    "pHR" int   NOT NULL,
-    "pTB" int   NOT NULL,
-    "pBB" int   NOT NULL,
-    "pIntBB" int   NOT NULL,
-    "pHBP" int   NOT NULL,
-    "pSF" int   NOT NULL,
-    CONSTRAINT "pk_Hitting_Stats" PRIMARY KEY (
-        "gameID","playerID"
-     )
-);
+TABLESPACE pg_default;
+
+ALTER TABLE public.hitting_stats
+    OWNER to postgres;
--- sql_scripts/views_v1/hitting_standard_view.sql
+++ sql_scripts/views_v1/hitting_standard_view.sql
@@ -1,8 +1,22 @@
 SELECT
 	hs."playerID",
 	plyr."teamID",
-	SUM(hs."pPlateApp") as "pPlateApps",
-	SUM(hs."pAtBats") as "pAtBats"
+	SUM(hs."pPlateApps") as "pPlateApps",
+	SUM(hs."pAtBats") as "pAtBats",
+	SUM(hs."pHits") as "pHits",
+	SUM(hs."pStrikeouts") as "pStrikeouts",
+	SUM(hs."pBB") as "pBBs",
+	(SUM(hs."pHits") - SUM(hs."p2B" + hs."p3B" + hs."pHR")) as "pSingles",
+	SUM(hs."p2B") as "pDoubles",
+	SUM(hs."p3B") as "pTriples",
+	SUM(hs."pHR") as "pHomeRuns",
+	SUM(hs."pTB") as "pTotalBases",
+	SUM(hs."pSF") as "pSacFlys",
+	SUM(hs."pHBP") as "pHBPs",
+	SUM(hs."pIntBB") as "pIBBs",
+	ROUND(CAST(SUM(hs."pHits")as numeric)/CAST(SUM(hs."pAtBats")as numeric),3) as "pBatAvg",
+	ROUND(CAST(SUM(hs."pTB")as numeric)/CAST(SUM(hs."pAtBats")as numeric),3) as "pSlugPer",
+	ROUND(CAST(SUM(hs."pHits" + hs."pBB" + hs."pHBP")as numeric)/CAST(SUM(hs."pPlateApps")as numeric),3) as "pOBP"
 FROM hitting_stats AS hs
 INNER JOIN player as plyr
 	ON hs."playerID" = plyr."playerID"
